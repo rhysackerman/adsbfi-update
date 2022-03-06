522864990cd02fff210cd643d4a7c11ee6313bed
Matthias Wirth <matthias.wirth@gmail.com>
2022-03-06 18:30:28 +0100

    add stats.prom output

--- skeleton/usr/local/bin/readsb.sh
+++ skeleton/usr/local/bin/readsb.sh
@@ -4,6 +4,6 @@ if [[ "$DUMP1090" == "no" ]]; then
     RECEIVER_OPTIONS="--net-only"
 fi
 
-exec /usr/bin/readsb --gain $GAIN --lat $LATITUDE --lon $LONGITUDE \
- $RECEIVER_OPTIONS $DECODER_OPTIONS --db-file=none $NET_OPTIONS $JSON_OPTIONS \
+exec /usr/bin/readsb --write-prom /run/readsb/stats.prom --gain $GAIN --lat $LATITUDE --lon $LONGITUDE \
+ $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $JSON_OPTIONS \
  --write-json /run/readsb --quiet
