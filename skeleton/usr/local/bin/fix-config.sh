5c446f555082929403800b8b8f27cc2298c92c52
Matthias Wirth <matthias.wirth@gmail.com>
2022-05-25 15:50:46 +0200

    disable mlat for fr24

--- skeleton/usr/local/bin/fix-config.sh
+++ skeleton/usr/local/bin/fix-config.sh
@@ -9,7 +9,8 @@ if grep -qs -e 'receiver.*dvb' /etc/fr24feed.ini; then
     if ! grep -e 'host=' /etc/fr24feed.ini &>/dev/null; then echo 'host=' >> /etc/fr24feed.ini; fi
     if ! grep -e 'receiver=' /etc/fr24feed.ini &>/dev/null; then echo 'receiver=' >> /etc/fr24feed.ini; fi
 
-    sed -i -e 's/receiver=.*/receiver="beast-tcp"/' -e 's/host=.*/host="127.0.0.1:30005"/' -e 's/bs=.*/bs="no"/' -e 's/raw=.*/raw="no"/' /etc/fr24feed.ini
+    sed -i -e 's/receiver=.*/receiver="beast-tcp"/' -e 's/host=.*/host="127.0.0.1:30005"/' \
+        -e 's/mlat=.*/mlat="no"/' -e 's/bs=.*/bs="no"/' -e 's/raw=.*/raw="no"/' /etc/fr24feed.ini
 
     pkill -9 fr24feed || true
     apt purge -y dump1090 &>/dev/null
